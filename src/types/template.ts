db1254b6bee5c36a616a4abcb1c425c5d711eb4c
That One Dino Guy <thelordofdino4@gmail.com>
2026-05-22 00:54:16 -0400

    Add template data types

---
+++ src/types/template.ts
@@ -0,0 +1,39 @@
+export interface DiscTemplate {
+  id: string
+  name: string
+  type: 'disc'
+  units: 'mm'
+  outerDiameterMm: number
+  innerHoleDiameterMm: number
+  printableDiameterMm: number
+  safeDiameterMm: number
+  bleedDiameterMm?: number
+  defaultZones: TemplateZone[]
+}
+
+export interface TemplateZone {
+  id: string
+  name: string
+  xMm: number
+  yMm: number
+  widthMm: number
+  heightMm: number
+  anchor: 'center' | 'top-left'
+}
+
+export interface TemplateGuide {
+  id: string
+  name: string
+  type: 'outerEdge' | 'centerHole' | 'safeZone' | 'bleedZone' | 'foldLine' | 'spineLine'
+  visibleByDefault: boolean
+}
+
+export interface TemplateMask {
+  id: string
+  name: string
+  type: 'circle' | 'rectangle'
+  xMm: number
+  yMm: number
+  widthMm: number
+  heightMm: number
+}
